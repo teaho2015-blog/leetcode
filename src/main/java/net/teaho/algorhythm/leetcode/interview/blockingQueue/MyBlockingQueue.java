aeac24171ba0f9d365e8ff35abf0fc67e6a81193
teaho2015 <teaho2015@gmail.com>
2021-06-20 04:46:53 +0800

    [add]自实现阻塞队列

---
+++ src/main/java/net/teaho/algorhythm/leetcode/interview/blockingQueue/MyBlockingQueue.java
@@ -0,0 +1,83 @@
+package net.teaho.algorhythm.leetcode.interview.blockingQueue;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * <p>百度面试题
+ * <p>自实现一个阻塞有界队列。
+ */
+public class MyBlockingQueue {
+
+    private final int[] vals;
+
+    private int capicity = 100;
+
+    private int curSize = 0;
+
+    /**
+     * 队列尾巴位置
+     */
+    private int slot = 0;
+
+    /**
+     * 主锁
+     */
+    private Lock lock = new ReentrantLock();
+    /**
+     * 队列为空
+     */
+    private Condition isNull = lock.newCondition();
+    /**
+     * 队列已满
+     */
+    private Condition isFull = lock.newCondition();
+
+    public MyBlockingQueue() {
+        vals = new int[capicity];
+    }
+
+    public MyBlockingQueue(int capicity) {
+        this.capicity = capicity;
+        vals = new int[this.capicity];
+    }
+
+
+    public void push(int val) throws InterruptedException {
+
+        try {
+            lock.lock();
+
+            while (curSize == capicity) {
+                isFull.await();
+            }
+            slot++;
+            curSize++;
+            vals[slot % capicity] = val;
+            isNull.signal();
+
+        } finally {
+            lock.unlock();
+        }
+
+
+    }
+
+    public int pop() throws InterruptedException {
+        try {
+            lock.lock();
+            while (curSize == 0) {
+                isNull.await();
+            }
+            curSize--;
+            int result = vals[(slot - curSize) % capicity];
+            isFull.signal();
+            return result;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+
+}
