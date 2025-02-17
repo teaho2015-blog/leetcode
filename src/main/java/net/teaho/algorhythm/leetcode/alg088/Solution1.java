27a7fb025be03bbd7d1f240bc19a854e7b64a752
teaho2015 <teaho2015@gmail.com>
2025-02-17 11:42:52 +0800

    add

---
+++ src/main/java/net/teaho/algorhythm/leetcode/alg088/Solution1.java
@@ -0,0 +1,16 @@
+package net.teaho.algorhythm.leetcode.alg088;
+
+/**
+ * @author teaho2015<at/>gmail.com
+ * @date 2025-02
+ */
+public class Solution1 {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+}
