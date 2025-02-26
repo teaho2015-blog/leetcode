de7f6deea43c5f05e78a76509dc6d6ca55b8ec89
teaho2015 <teaho2015@gmail.com>
2025-02-27 01:38:18 +0800

    [upd]alg253
    Signed-off-by: teaho2015 <teaho2015@gmail.com>

---
+++ src/main/java/net/teaho/algorhythm/leetcode/alg253/Solution.java
@@ -0,0 +1,20 @@
+package net.teaho.algorhythm.leetcode.alg253;
+
+/**
+ *
+ * <p>279. 完全平方数
+ * https://leetcode.cn/problems/meeting-rooms-ii/solutions/2392308/hui-yi-shi-ii-by-leetcode-solution-fzxq/?envType=problem-list-v2&envId=2cktkvj
+ *
+ * <p>时间复杂度：
+ * <p>空间复杂度：
+ *
+ * @author teaho2015@gmail.com
+ * @date 2025-02
+ */
+public class Solution {
+
+    public int minMeetingRooms(int[][] intervals) {
+        return 0;
+    }
+
+}
