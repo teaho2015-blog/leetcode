1bf81b5b348780557a04432dae95a61aeb4c8275
teaho2015 <teaho2015@gmail.com>
2025-03-05 19:49:50 +0800

    upd

---
+++ src/main/java/net/teaho/algorhythm/leetcode/alg739/Solution.java
@@ -0,0 +1,34 @@
+package net.teaho.algorhythm.leetcode.alg739;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 739. 每日温度 <p>
+ * https://leetcode.cn/problems/daily-temperatures/description/?envType=problem-list-v2&envId=2cktkvj&
+ *
+ *  <p>时间复杂度：
+ *  <p>空间复杂度：
+ *
+ * @author teaho2015@gmail.com
+ * @date 2025-03
+ */
+public class Solution {
+
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        int length = temperatures.length;
+        int[] res = new int[length];
+        Deque<Integer> stack = new LinkedList<>();
+        for (int i = 0; i < temperatures.length; i++) {
+            int temperature = temperatures[i];
+            while (!stack.isEmpty() && temperature > temperatures[stack.peek()]) {
+                int pre = stack.pop();
+                res[pre] = i - pre;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+
+}
