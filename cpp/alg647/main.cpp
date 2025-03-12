2207c0bd2141c64dc366074f5c9c7c98e11358f5
teaho2015 <teaho2015@gmail.com>
2025-03-12 15:54:32 +0800

    alg647

---
+++ cpp/alg647/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <thread>
+#include "../common/IntTreeNode.h"
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::to_string;
+
+
+
+/*
+647. 回文子串
+https://leetcode.cn/problems/palindromic-substrings/description/?envType=problem-list-v2&envId=2cktkvj&
+
+
+时间复杂度：O(n)
+空间复杂度：O(n)
+
+Created by teaho on 25-03-12
+*/
+class Solution {
+public:
+    int countSubstrings(string s) {
+        return 0;
+    }
+};
+
+int main()
+{
+
+
+
+}
